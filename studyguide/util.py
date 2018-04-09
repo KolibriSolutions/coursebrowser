dbc41449e78503f29d2ad3ace6cbbed651031771
Frank Boerman <frank@fboerman.nl>
2018-04-09 21:27:17 +0200

    fixed path converting to key for weird chars

--- studyguide/util.py
+++ studyguide/util.py
@@ -1,4 +1,6 @@
+from urllib.parse import unquote_plus
 
 def get_path_key(path):
+   path = unquote_plus(path)
    key = 'render_page_{}'.format(path.split('?')[0].strip('/').replace('/', '_').replace('&', '_'))
    return key
